b4ba38233b1c8cb421753b743ff18b23a850c92f
Sammy Sammon <anonymous-thing@hotmail.com>
2017-11-14 21:16:22 +1000

    Fix double click / item check event collision

--- InjectMeDaddy/ThemedListView.cs
+++ InjectMeDaddy/ThemedListView.cs
@@ -9,6 +9,8 @@ namespace InjectMeDaddy
 {
 	public class ThemedListView : ListView
 	{
+		private bool IgnoreNextCheck = false;
+
 		[DllImport("uxtheme.dll", CharSet = CharSet.Unicode)]
 		public static extern int SetWindowTheme(IntPtr hWnd, String pszSubAppName, String pszSubIdList);
 
@@ -23,5 +25,31 @@ namespace InjectMeDaddy
 			base.OnHandleCreated(e);
 			SetWindowTheme(this.Handle, "explorer", null);
 		}
-    }
+
+		protected override void OnItemCheck(ItemCheckEventArgs ice)
+		{
+			if (IgnoreNextCheck)
+			{
+				ice.NewValue = ice.CurrentValue;
+				IgnoreNextCheck = false;
+			}
+			else
+			{
+				base.OnItemCheck(ice);
+			}
+		}
+
+		protected override void OnMouseDown(MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left && e.Clicks > 1)
+				IgnoreNextCheck = true;
+			base.OnMouseDown(e);
+		}
+
+		protected override void OnKeyDown(KeyEventArgs e)
+		{
+			IgnoreNextCheck = false;
+			base.OnKeyDown(e);
+		}
+	}
 }
